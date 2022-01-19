5b11ab68309cf3d5f02d51d052e2070ec7929669
AB <96754248+quantum-hash@users.noreply.github.com>
2022-01-19 01:34:36 +0100

    Update and rename last.py to main_last.py

--- main_last.py
+++ main_last.py
@@ -4,72 +4,7 @@
 #connection_checker.check(url = "https://www.amazon.com", frequency = [240,300])
 
 import telebot, ffmpeg, time, os, requests
-from pytube import YouTube
 from telebot import types
-
-def streams(ink, only_audio=False): #ink : link of the video
-		global video
-		if only_audio == True:
-			comp2 = {}
-
-			youtube = YouTube(ink)
-			video = youtube.streams.filter(only_audio=True)
-			for i in list(enumerate(video)):
-				#print(dir(i[1]))
-				valu_e = int(i[1].filesize)/1024/1024
-				if valu_e > 1000 :
-					valu_e = str(round(valu_e/1024, 2))+ " GB"
-				else:
-					valu_e = str(round(valu_e, 1))+ " MB"
-				#print(valu_e)
-				ext = i[1].subtype
-				itag = i[1].itag
-				
-				if (ext == "webm") or (ext == "mp4"):
-					bits = int(i[1].bitrate)
-					if ext in comp2:
-						if bits > comp2[ext][1]:
-							comp2[ext] = [itag, bits, i[1].audio_codec, valu_e]
-					else:
-						comp2[ext] = [itag, bits, i[1].audio_codec, valu_e]
-			return comp2
-
-		if only_audio == False:
-			comp = [[],{}]
-			comp2 = {}
-
-			youtube = YouTube(ink)
-			video = youtube.streams
-			for i in list(enumerate(video)):
-				#print(dir(i[1]))
-				valu_e = int(i[1].filesize)/1024/1024
-				if valu_e > 1000 :
-					valu_e = str(round(valu_e/1024, 2))+ " GB"
-				else:
-					valu_e = str(round(valu_e, 1))+ " MB"
-				#print(valu_e)
-				ext = i[1].subtype
-				itag = i[1].itag
-				if i[1].type == "video" and (ext == "webm" or ext == "mp4"):
-					pixel = int(i[1].resolution[:-1])
-		
-					comp[0].append(pixel)
-				
-					if pixel in comp[1]:
-						if (i[1].is_progressive == True) and (ext == "mp4"):
-							comp[1][pixel] = [itag, i[1].resolution, ext, i[1].is_progressive, i[1].video_codec, valu_e]
-					else:
-						comp[1][pixel] = [itag, i[1].resolution, ext, i[1].is_progressive, i[1].video_codec, valu_e]
-				if i[1].type == "audio" and (ext == "webm" or ext == "mp4"):
-		
-					bits = int(i[1].bitrate)
-					if ext in comp2:
-						if bits > comp2[ext][1]:
-							comp2[ext] = [itag, bits, i[1].audio_codec, valu_e]
-					else:
-						comp2[ext] = [itag, bits, i[1].audio_codec, valu_e]
-			comp[0] = sorted(list(set(comp[0])))
-			return comp, comp2
 
 #------------------------------------------------------------------------------------------------------------
 
